57971659c74775ffcb7213cfbef813f05feeff0d
Marina Murashev <marina@hoteltonight.com>
2015-02-21 18:58:57 -0800

    customizes twitter timeline array adapter view

--- app/src/main/java/com/codepath/apps/mysimpletweets/adapters/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/adapters/TweetsArrayAdapter.java
@@ -1,15 +1,44 @@
 package com.codepath.apps.mysimpletweets.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.codepath.apps.mysimpletweets.models.User;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
 
     public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
-        super(context, android.R.layout.simple_list_item_1, tweets);
+        super(context, 0, tweets);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Tweet tweet = getItem(position);
+        User user = tweet.getUser();
+        
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+        }
+
+        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        
+        tvUserName.setText(user.getScreenName());
+        tvBody.setText(tweet.getBody());
+        ivProfileImage.setImageResource(android.R.color.transparent);
+        Picasso.with(getContext()).load(user.getProfileImageUrl()).into(ivProfileImage);
+        
+        return convertView;
     }
 }
